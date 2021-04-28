b635b34b1789fd4746c1af20a79c84944af59ea8
Christopher R Puente <me@chrispuente.com>
2021-04-27 21:33:53 -0500

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,239 @@
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+*/
+
+jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});
+
+
+/**
+ * Copyright (c) 2007 Ariel Flesler - aflesler ○ gmail • com | https://github.com/flesler
+ * Licensed under MIT
+ * @author Ariel Flesler
+ * @version 2.1.2
+ */
+;(function(f){"use strict";"function"===typeof define&&define.amd?define(["jquery"],f):"undefined"!==typeof module&&module.exports?module.exports=f(require("jquery")):f(jQuery)})(function($){"use strict";function n(a){return!a.nodeName||-1!==$.inArray(a.nodeName.toLowerCase(),["iframe","#document","html","body"])}function h(a){return $.isFunction(a)||$.isPlainObject(a)?a:{top:a,left:a}}var p=$.scrollTo=function(a,d,b){return $(window).scrollTo(a,d,b)};p.defaults={axis:"xy",duration:0,limit:!0};$.fn.scrollTo=function(a,d,b){"object"=== typeof d&&(b=d,d=0);"function"===typeof b&&(b={onAfter:b});"max"===a&&(a=9E9);b=$.extend({},p.defaults,b);d=d||b.duration;var u=b.queue&&1<b.axis.length;u&&(d/=2);b.offset=h(b.offset);b.over=h(b.over);return this.each(function(){function k(a){var k=$.extend({},b,{queue:!0,duration:d,complete:a&&function(){a.call(q,e,b)}});r.animate(f,k)}if(null!==a){var l=n(this),q=l?this.contentWindow||window:this,r=$(q),e=a,f={},t;switch(typeof e){case "number":case "string":if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(e)){e= h(e);break}e=l?$(e):$(e,q);case "object":if(e.length===0)return;if(e.is||e.style)t=(e=$(e)).offset()}var v=$.isFunction(b.offset)&&b.offset(q,e)||b.offset;$.each(b.axis.split(""),function(a,c){var d="x"===c?"Left":"Top",m=d.toLowerCase(),g="scroll"+d,h=r[g](),n=p.max(q,c);t?(f[g]=t[m]+(l?0:h-r.offset()[m]),b.margin&&(f[g]-=parseInt(e.css("margin"+d),10)||0,f[g]-=parseInt(e.css("border"+d+"Width"),10)||0),f[g]+=v[m]||0,b.over[m]&&(f[g]+=e["x"===c?"width":"height"]()*b.over[m])):(d=e[m],f[g]=d.slice&& "%"===d.slice(-1)?parseFloat(d)/100*n:d);b.limit&&/^\d+$/.test(f[g])&&(f[g]=0>=f[g]?0:Math.min(f[g],n));!a&&1<b.axis.length&&(h===f[g]?f={}:u&&(k(b.onAfterFirst),f={}))});k(b.onAfter)}})};p.max=function(a,d){var b="x"===d?"Width":"Height",h="scroll"+b;if(!n(a))return a[h]-$(a)[b.toLowerCase()]();var b="client"+b,k=a.ownerDocument||a.document,l=k.documentElement,k=k.body;return Math.max(l[h],k[h])-Math.min(l[b],k[b])};$.Tween.propHooks.scrollLeft=$.Tween.propHooks.scrollTop={get:function(a){return $(a.elem)[a.prop]()}, set:function(a){var d=this.get(a);if(a.options.interrupt&&a._last&&a._last!==d)return $(a.elem).stop();var b=Math.round(a.now);d!==b&&($(a.elem)[a.prop](b),a._last=this.get(a))}};return p});
+
+/*
+ * jQuery One Page Nav Plugin
+ * http://github.com/davist11/jQuery-One-Page-Nav
+ *
+ * Copyright (c) 2010 Trevor Davis (http://trevordavis.net)
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://jquery.org/license
+ *
+ * @version 2.1
+ *
+ * Example usage:
+ * $('#nav').onePageNav({
+ *   currentClass: 'current',
+ *   changeHash: false,
+ *   scrollSpeed: 750
+ * });
+ */
+
+;(function($, window, document, undefined){
+
+// our plugin constructor
+var OnePageNav = function(elem, options){
+this.elem = elem;
+this.$elem = $(elem);
+this.options = options;
+this.metadata = this.$elem.data('plugin-options');
+this.$nav = this.$elem.find('a');
+this.$win = $(window);
+this.sections = {};
+this.didScroll = false;
+this.$doc = $(document);
+this.docHeight = this.$doc.height();
+};
+
+// the plugin prototype
+OnePageNav.prototype = {
+defaults: {
+currentClass: 'current',
+changeHash: false,
+easing: 'swing',
+filter: '',
+scrollSpeed: 750,
+scrollOffset: 0,
+scrollThreshold: 0.5,
+begin: false,
+end: false,
+scrollChange: false
+},
+
+init: function() {
+var self = this;
+
+// Introduce defaults that can be extended either
+// globally or using an object literal.
+self.config = $.extend({}, self.defaults, self.options, self.metadata);
+
+//Filter any links out of the nav
+if(self.config.filter !== '') {
+self.$nav = self.$nav.filter(self.config.filter);
+}
+
+//Handle clicks on the nav
+self.$nav.on('click.onePageNav', $.proxy(self.handleClick, self));
+
+//Get the section positions
+self.getPositions();
+
+//Handle scroll changes
+self.bindInterval();
+
+//Update the positions on resize too
+self.$win.on('resize.onePageNav', $.proxy(self.getPositions, self));
+
+return this;
+},
+
+adjustNav: function(self, $parent) {
+self.$elem.find('.' + self.config.currentClass).removeClass(self.config.currentClass);
+$parent.addClass(self.config.currentClass);
+},
+
+bindInterval: function() {
+var self = this;
+var docHeight;
+
+self.$win.on('scroll.onePageNav', function() {
+self.didScroll = true;
+});
+
+self.t = setInterval(function() {
+docHeight = self.$doc.height();
+
+//If it was scrolled
+if(self.didScroll) {
+self.didScroll = false;
+self.scrollChange();
+}
+
+//If the document height changes
+if(docHeight !== self.docHeight) {
+self.docHeight = docHeight;
+self.getPositions();
+}
+}, 250);
+},
+
+getHash: function($link) {
+return $link.attr('href').split('#')[1];
+},
+
+getPositions: function() {
+var self = this;
+var linkHref;
+var topPos;
+var $target;
+
+self.$nav.each(function() {
+linkHref = self.getHash($(this));
+$target = $('#' + linkHref);
+
+if($target.length) {
+topPos = $target.offset().top;
+self.sections[linkHref] = Math.round(topPos) - self.config.scrollOffset;
+}
+});
+},
+
+getSection: function(windowPos) {
+var returnValue = null;
+var windowHeight = Math.round(this.$win.height() * this.config.scrollThreshold);
+
+for(var section in this.sections) {
+if((this.sections[section] - windowHeight) < windowPos) {
+returnValue = section;
+}
+}
+
+return returnValue;
+},
+
+handleClick: function(e) {
+var self = this;
+var $link = $(e.currentTarget);
+var $parent = $link.parent();
+var newLoc = '#' + self.getHash($link);
+
+if(!$parent.hasClass(self.config.currentClass)) {
+//Start callback
+if(self.config.begin) {
+self.config.begin();
+}
+
+//Change the highlighted nav item
+self.adjustNav(self, $parent);
+
+//Removing the auto-adjust on scroll
+self.unbindInterval();
+
+//Scroll to the correct position
+$.scrollTo(newLoc, self.config.scrollSpeed, {
+axis: 'y',
+easing: self.config.easing,
+offset: {
+top: -self.config.scrollOffset
+},
+onAfter: function() {
+//Do we need to change the hash?
+if(self.config.changeHash) {
+window.location.hash = newLoc;
+}
+
+//Add the auto-adjust on scroll back in
+self.bindInterval();
+
+//End callback
+if(self.config.end) {
+self.config.end();
+}
+}
+});
+}
+
+e.preventDefault();
+},
+
+scrollChange: function() {
+var windowTop = this.$win.scrollTop();
+var position = this.getSection(windowTop);
+var $parent;
+
+//If the position is set
+if(position !== null) {
+$parent = this.$elem.find('a[href$="#' + position + '"]').parent();
+
+//If it's not already the current section
+if(!$parent.hasClass(this.config.currentClass)) {
+//Change the highlighted nav item
+this.adjustNav(this, $parent);
+
+//If there is a scrollChange callback
+if(this.config.scrollChange) {
+this.config.scrollChange($parent);
+}
+}
+}
+},
+
+unbindInterval: function() {
+clearInterval(this.t);
+this.$win.unbind('scroll.onePageNav');
+}
+};
+
+OnePageNav.defaults = OnePageNav.prototype.defaults;
+
+$.fn.onePageNav = function(options) {
+return this.each(function() {
+new OnePageNav(this, options).init();
+});
+};
+
+})( jQuery, window , document );
